b64952c3966a1645b49440dc8471fc21ebce27ae
ESchulstad <eschulstad@gmail.com>
2016-11-15 11:54:45 -0500

    got donations to work

--- GotFood/Controllers/ScoreBoardController.cs
+++ GotFood/Controllers/ScoreBoardController.cs
@@ -20,6 +20,7 @@ namespace GotFood.Controllers
         // GET: ScoreBoard
         public ActionResult Index()
         {
+            ViewBag.Level = "1";
             var providers = db.Providers.Include(p => p.ProviderTransportation).Include(p => p.ProviderType);
 
             //Provider provider = db.Providers.Find(id);
@@ -43,16 +44,70 @@ namespace GotFood.Controllers
                 return HttpNotFound();
             }
 
-            int donations = provider.NumOfDonation;
-            donations += 1;
+           // int donations = provider.NumOfDonation;
+            //int newNum = donations++;
+            //newNum = provider.NumOfDonation;
+            provider.NumOfDonation++;
+            //db.Entry(provider).State = EntityState.Modified;
 
-            db.SaveChanges();
 
-            if(donations >= 0 && donations<= 5)
+            if (ModelState.IsValid)
             {
-                ViewBag.Level = 1;
+                db.Entry(provider).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
             }
+            
             return RedirectToAction("Index");
         }
+
+        public ActionResult NumOfDonation(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Provider provider = db.Providers.Find(id);
+            if (provider == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            if (provider.NumOfDonation >= 0 && provider.NumOfDonation <= 5)
+            {
+                ViewBag.Level = "1";
+            }
+            else if (provider.NumOfDonation > 5 && provider.NumOfDonation<=12)
+            {
+                ViewBag.Level = "2";
+            }
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        // GET: Providers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Provider provider = db.Providers.Find(id);
+            if (provider == null)
+            {
+                return HttpNotFound();
+            }
+            if (provider.NumOfDonation >= 0 && provider.NumOfDonation <= 5)
+            {
+                ViewBag.Level = "1";
+            }
+            else if (provider.NumOfDonation > 5 && provider.NumOfDonation <= 12)
+            {
+                ViewBag.Level = "2";
+            }
+            return View(provider);
+        }
     }
+
 };
